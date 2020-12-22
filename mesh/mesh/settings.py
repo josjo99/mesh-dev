909054b48a6a9f8f68af0336ff2284a4b3d489fd
josjo99 <joshjon2017@gmail.com>
2020-12-22 14:39:13 +0530

    modified DATABASES in settings.py

--- mesh/mesh/settings.py
+++ mesh/mesh/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'meshdb',
         'USER': 'meshuser',
-        'PASS': 'meshpass',
+        'PASSWORD': 'meshpass',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
