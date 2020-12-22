dfb7220701fc0cd5b0f71e7150123038d5f79789
josjo99 <joshjon2017@gmail.com>
2020-12-22 14:37:35 +0530

    modified DATABASES in settings.py to 'default'

--- mesh/mesh/settings.py
+++ mesh/mesh/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mesh.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'postgres': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'meshdb',
         'USER': 'meshuser',
