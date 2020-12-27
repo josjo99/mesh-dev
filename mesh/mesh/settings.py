fd83f284a5bed476cb3aa766ce0f606ae484a690
josjo99 <joshjon2017@gmail.com>
2020-12-27 13:32:37 +0530

    settings.py typo

--- mesh/mesh/settings.py
+++ mesh/mesh/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'mesh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'meshdb',
+        'NAME': 'mesh',
         'USER': 'meshuser',
         'PASSWORD': 'meshpass',
         'HOST': '127.0.0.1',
