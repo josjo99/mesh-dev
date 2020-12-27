068ece84f40fa915f41024c51086868320fe6448
josjo99 <joshjon2017@gmail.com>
2020-12-27 13:26:02 +0530

    models typo

--- mesh/main/models.py
+++ mesh/main/models.py
@@ -3,24 +3,24 @@ from django.db import models
 # Create your models here.
 
 class Channel(models.Model):
-    channel_id= models.CharField(_("Channel Id"), max_length=50)
-    channel_name= models.CharField(_("Channel Name"), max_length=50)
+    channel_id= models.CharField("Channel Id", max_length=50)
+    channel_name= models.CharField("Channel Name", max_length=50)
 
     class Meta:
-        verbose_name = _("Channel")
-        verbose_name_plural = _("Channels")
+        verbose_name = "Channel"
+        verbose_name_plural = "Channels"
 
     def __str__(self):
         return self.name
 
 
 class Group(models.Model):
-    group_id= models.CharField(_("Group Id"), max_length=50)
-    group_name= models.CharField(_("Group Name"), max_length=50)
+    group_id= models.CharField("Group Id", max_length=50)
+    group_name= models.CharField("Group Name", max_length=50)
 
     class Meta:
-        verbose_name = _("Group")
-        verbose_name_plural = _("Groups")
+        verbose_name = "Group"
+        verbose_name_plural = "Groups"
 
     def __str__(self):
         return self.name
