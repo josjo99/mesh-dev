3c5db4bc55f2bdb7f077c14680842cfec56718ef
josjo99 <joshjon2017@gmail.com>
2020-12-27 13:13:41 +0530

    database mods

--- mesh/authent/models.py
+++ mesh/authent/models.py
@@ -26,27 +26,3 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Channel(models.Model):
-    channel_id= models.CharField(_("Channel Id"), max_length=50)
-    channel_name= models.CharField(_("Channel Name"), max_length=50)
-
-    class Meta:
-        verbose_name = _("Channel")
-        verbose_name_plural = _("Channels")
-
-    def __str__(self):
-        return self.name
-
-
-class Group(models.Model):
-    group_id= models.CharField(_("Group Id"), max_length=50)
-    group_name= models.CharField(_("Group Name"), max_length=50)
-
-    class Meta:
-        verbose_name = _("Group")
-        verbose_name_plural = _("Groups")
-
-    def __str__(self):
-        return self.name
--- mesh/main/models.py
+++ mesh/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Channel(models.Model):
+    channel_id= models.CharField(_("Channel Id"), max_length=50)
+    channel_name= models.CharField(_("Channel Name"), max_length=50)
+
+    class Meta:
+        verbose_name = _("Channel")
+        verbose_name_plural = _("Channels")
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    group_id= models.CharField(_("Group Id"), max_length=50)
+    group_name= models.CharField(_("Group Name"), max_length=50)
+
+    class Meta:
+        verbose_name = _("Group")
+        verbose_name_plural = _("Groups")
+
+    def __str__(self):
+        return self.name
