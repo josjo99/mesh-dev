3601a6afaf70f9b7b85910a6f5132a9659ac75f0
josjo99 <joshjon2017@gmail.com>
2021-01-03 22:40:54 +0530

    models

--- mesh/authentication/admin.py
+++ mesh/authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import User,UserClass
 # Register your models here.
+
+admin.site.register(User,UserClass)
--- mesh/authentication/models.py
+++ mesh/authentication/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class UserClass(models.Model):
+    userclassname = models.CharField("User Class Name", max_length=50)
+
+    class Meta:
+        verbose_name = "UserClass"
+        verbose_name_plural = "UserClasses"
+    
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    email = models.EmailField("Email", max_length=254)
+    password = models.CharField("Password",max_length=50)
+    userclass = models.ManyToManyField("UserClass", "User Class")
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.name
