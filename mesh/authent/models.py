9ce661b60bced8f0fdb4e2190cf32447308babf7
root <root@localhost.localdomain>
2021-01-03 10:08:23 +0000

    models

--- mesh/authent/models.py
+++ mesh/authent/models.py
@@ -5,7 +5,7 @@ from django.db.models.deletion import PROTECT
 # Create your models here.
 
 class UserClass(models.Model):
-    user_class_name = models.CharField("User Class", max_length=50,)
+    user_class_name = models.CharField("User Class", max_length=50)
 
 class User(AbstractUser):
-    user_class = models.ManyToManyField(UserClass, verbose_name="User Class",on_delete=PROTECT)
+    user_class = models.ManyToManyField(UserClass, verbose_name="User Class")
