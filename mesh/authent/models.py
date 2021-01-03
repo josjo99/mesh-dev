6fc7975751f0f800b60bce6fdde8d26d0cf851d6
josjo99 <joshjon2017@gmail.com>
2021-01-03 15:22:22 +0530

    modified models

--- mesh/authent/models.py
+++ mesh/authent/models.py
@@ -1,10 +1,11 @@
 from typing import AbstractSet
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import PROTECT
 # Create your models here.
 
 class UserClass(models.Model):
-    user_class_name = models.CharField("User Class", max_length=50)
+    user_class_name = models.CharField("User Class", max_length=50,)
 
 class User(AbstractUser):
-    user_class = models.ManyToManyField("UserClass", verbose_name="User Class")
+    user_class = models.ManyToManyField(UserClass, verbose_name="User Class",on_delete=PROTECT)
