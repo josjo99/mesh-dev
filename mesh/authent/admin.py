ab5483f03dd394bfbf6fe1c12869d6015a409914
josjo99 <joshjon2017@gmail.com>
2021-01-03 19:42:24 +0530

    models

--- mesh/authent/admin.py
+++ mesh/authent/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MeshUser,UserClass
+from .models import MeshUser,MeshUserClass
 # Register your models here.
 
-admin.site.register(MeshUser,UserClass)
+admin.site.register(MeshUser)
+admin.site.register(MeshUserClass)
