666526a8f04a1e418a0e244169801abed71aeca7
josjo99 <joshjon2017@gmail.com>
2021-01-03 15:36:17 +0530

    models

--- mesh/authent/models.py
+++ mesh/authent/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class UserClass(models.Model):
-    user_class_name = models.CharField("User Class", max_length=50,)
+    user_class_name = models.CharField("User Class", max_length=50)
+
+    class Meta:
+        verbose_name = "UserClass"
+        verbose_name_plural = "UserClasses"
+
+    def __str__(self):
+        return self.name
+    
 
 class User(AbstractUser):
     user_class = models.ManyToManyField(UserClass, verbose_name="User Class")
